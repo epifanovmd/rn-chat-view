a7953c7cbafccc3064bc60c10eb63e3307065813
Андрей Епифанов <epifanovmd@gmail.com>
2026-04-02 23:08:00 +0300

    Initial Commit

---
+++ rn-chat-view/rn_chat_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  rn_chat_viewApp.swift
+//  rn-chat-view
+//
+//  Created by Andrei on 02.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct rn_chat_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
